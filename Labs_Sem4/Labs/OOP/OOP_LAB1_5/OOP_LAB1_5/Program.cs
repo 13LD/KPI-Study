ed0d31bc07b14fd46674fececb0ae453e890934e
Dmitry Lysogor <tompla96@ukr.net>
2016-05-17 22:37:03 +0300

    euler tasks
    euler tasks

--- Labs_Sem4/Labs/OOP/OOP_LAB1_5/OOP_LAB1_5/Program.cs
+++ Labs_Sem4/Labs/OOP/OOP_LAB1_5/OOP_LAB1_5/Program.cs
@@ -1,82 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace OOP_LAB1_5
+namespace Rextester
 {
-	public interface IHandler{
-		HandlerBase Successor { set;}
-	}
 
-	abstract public class HandlerBase
-	{
-		protected HandlerBase _successor;
-		public HandlerBase Successor{
-			set { _successor = value; } 
-		}
-		abstract public string ShowUser();
-		abstract public void Rights();
-	}
-
-	public class ConcreteHandler1 : HandlerBase
-	{
-		public override string ShowUser(){
-			return String.Format("Simple user");
-		} 
-		public override void Rights(){
-			Console.WriteLine("Simple user, nothing to show");
-		} 
-	}
-	public class ConcreteHandler2 : HandlerBase
-	{
-		public override string ShowUser(){
-			return String.Format("\nAdvanced user");
-		} 
-		public override void Rights() {
-			Console.WriteLine("\nChoose method\n1.Show information \n2.Input information to list \n3.Show users");
-			Console.ReadLine ();
-			Console.WriteLine ("No rights to show something, advanced user");
-		} 
-	}
-
-	public class ConcreteHandler3 : HandlerBase
-	{
-		public override string ShowUser(){
-			return String.Format("\nadmin ");
-		} 
-		public override void Rights(){
-			List<string> list_info = new List<string> ();
-			List<string> list_users = new List<string> ();
-
-			Console.WriteLine("\nChoose method\n1.Show information \n2.Input information to list \n3.Show users");
-			string a = Console.ReadLine ();
-			if (a == "1")
-				Console.WriteLine ("You have just entered admin room");
-			else if (a == "2") {
-				Console.WriteLine ("\nList of info");
-				list_info.Add ("System information");
-				list_info.Add ("3rd is your info");
-				foreach (string list in list_info)
-					Console.WriteLine (list);
-				Console.WriteLine ("Enter your info");
-				string b = Console.ReadLine ();
-				list_info.Add (b);
-				Console.WriteLine ("\nList of info");
-				foreach (string list in list_info)
-					Console.WriteLine (list);
-			} else if (a == "3") {
-				list_users.Add ("user1");
-				list_users.Add ("user2");
-				Console.WriteLine ("List of users");
-				foreach (string list in list_users)
-					Console.WriteLine (list);
-			}
-			else
-				Console.WriteLine ("Wrong input");
-		}
-	}
-
-
-	class Pupil
+    class Pupil
 	{
 		public void SetExecutor(Task t, Executor e)
 		{
@@ -135,10 +65,11 @@ namespace OOP_LAB1_5
 		}
 	}
 
-	class MainClass
-	{
-		public static void Task1(){
-			Console.WriteLine ("Task1\n");
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
 			Mother mother = new Mother();
 			Father father = new Father();
 			Granny granny = new Granny();
@@ -155,26 +86,6 @@ namespace OOP_LAB1_5
 			pupil[2].SetExecutor(tasks[2], granny);
 			for (int i=0;i<3;i++)
 				pupil[i].ExecuteTaskByHimself(tasks[i]);
-			Console.ReadKey();
-		}
-		public static void Task2(){
-			Console.WriteLine ("\nTask2\n");
-			HandlerBase c = new ConcreteHandler1 ();
-			Console.WriteLine (c.ShowUser ());
-			c.Rights ();
-
-			HandlerBase c1 = new ConcreteHandler2 ();
-			Console.WriteLine (c1.ShowUser ());
-			c1.Rights ();
-
-			HandlerBase c2 = new ConcreteHandler3 ();
-			Console.WriteLine (c2.ShowUser ());
-			c2.Rights ();
-		} 
-		public static void Main (string[] args)
-		{
-			Task1 ();
-			Task2 ();
-		}
-	}
+        }
+    }
 }
